b59063e47021ccdfde4c448984101a507d813123
Kostas <langas.kostya@mail.ru>
2022-08-03 17:20:42 +0500

    Нахождение расстояния между точками в 3д

---
+++ Exc21/Program.cs
@@ -0,0 +1,9 @@
+﻿int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+int stepen = 2;
+double n = Math.Sqrt((Math.Pow(x2-x1, stepen)) + (Math.Pow(y2-y1, stepen)) + (Math.Pow(z2-z1, stepen)));
+Console.WriteLine(n);
