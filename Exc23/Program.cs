13b350a6700a4c2a3abfccfa18a8c9216cdac1bb
Kostas <langas.kostya@mail.ru>
2022-08-03 17:24:18 +0500

    Возведение в куб от 1 до n

---
+++ Exc23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int stepen = 3;
+int i = 1;
+
+while (i <= n )
+{
+    Console.WriteLine(Math.Pow(i,stepen));
+    i++;
+
+}
